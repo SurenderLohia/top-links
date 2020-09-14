4a431376c987a49381e4d9b0beeb72235cc7288b
SurenderLohia <surender.lohia.e@gmail.com>
2020-09-14 20:05:00 +0530

    Add design to login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,13 +1,23 @@
 import React from "react";
 
+import signInWithTwitterBtn from "../images/sign-in-with-twitter-btn.png";
+
 function LoginPage() {
   const onLogin = () => {
     window.open("http://localhost:4000/auth/twitter", "_self");
   };
   return (
-    <div>
-      <h1>Login page 2</h1>
-      <button onClick={onLogin}>Login via Twitter</button>
+    <div className="login-container">
+      <div class="login-box">
+        <h1 className="is-size-4 mb-4">Top Links Login</h1>
+        <div className="box">
+          <div className="box-content">
+            <button className="btn-reset" onClick={onLogin}>
+              <img src={signInWithTwitterBtn} alt="Sign in with Twitter" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
